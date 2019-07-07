f69f478e3af99ba85fa0e781abe1d3d8fb428f81
gabriel1379 <gabriel1379@gmail.com>
2019-07-07 14:08:40 +0200

    Make formatting more structured

--- src/resultsformatter.py
+++ src/resultsformatter.py
@@ -23,17 +23,35 @@ class ResultsFormatter:
         return "".join(results_formatted)
 
     def __compose_header(self, group_name: str, num_files_in_group: int, group_results: List[str]) -> None:
-        group_header = "-----------\n" \
-                       f"Group '{group_name}': {num_files_in_group} members.\n" \
-                       "-----------\n"
-        group_results.append(group_header)
+        members_word_form = self.__determine_word_form("member", num_files_in_group)
+
+        group_header = f"Group '{group_name}': {num_files_in_group} {members_word_form}."
+        group_header_with_frame = self.__frame_line(group_header)
+
+        group_results.append(group_header_with_frame)
+
+    def __determine_word_form(self, word: str, number: int) -> str:
+        if number > 1:
+            word = f"{word}s"
+
+        return word
+
+    def __frame_line(self, line: str) -> str:
+        line_length = len(line)
+        line_frame = f"{line_length * '-'}"
+        line_with_frame = f"{line_frame}\n{line}\n{line_frame}\n"
+
+        return line_with_frame
 
     def __compose_file_list(self, files: List['Path'], group_results: List[str], num_files_in_group: int) -> None:
-        if num_files_in_group == 1:
-            group_results.append(f"File: {files[0]}\n")
-            return
+        title = "Files:\n"
 
-        group_results.append("Files:\n")
+        if num_files_in_group == 1:
+            title = "File:\n"
+
+        group_results.append(title)
 
         for file in files:
             group_results.append(f"{str(file)}\n")
+
+        group_results.append("\n")
